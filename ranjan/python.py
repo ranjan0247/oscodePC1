374112aca228c767bb555db7934dec01e17839f6
ranjan0247 <ranjanmamadapur@gmail.com>
2025-12-17 14:41:15 +0530

    Updated my code

--- ranjan/python.py
+++ ranjan/python.py
@@ -1,67 +1,123 @@
-import math
 import cv2
-from cvzone.HandTrackingModule import HandDetector
-from cvzone.ClassificationModule import Classifier
+import mediapipe as mp
 import numpy as np
+import math
 import time
+import os
+
+
+mp_hands = mp.solutions.hands
+hands_detector = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5)
+mp_draw = mp.solutions.drawing_utils
 
 
 cap = cv2.VideoCapture(0)
-detector= HandDetector(maxHands=1)
-classifier = Classifier("Model/keras_model.h5","Model/labels.txt")
 
 offset = 20
 imageSize = 300
 
-folder="Data/A"
-counter=0
+folder = "Data/A"
+counter = 0
 
-labels = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-while(True):
+if not os.path.exists(folder):
+    os.makedirs(folder)
+    print(f"Created directory: {folder}")
+
+print("Press 's' to save an image, 'q' to quit.")
+
+while True:
     success, img = cap.read()
-    hands, img = detector.findHands(img)
-    if hands:
-        hand = hands[0]
-        x, y, w, h = hand['bbox']
-        
-        imgWhite = np.ones((imageSize, imageSize, 3), np.uint8) * 255
-        imgCrop = img[y-offset:y+h+offset, x-offset:x+w+offset]
-        
-        imgCropShape = imgCrop.shape
-        
-        imgWhite[0:imgCrop.shape[0],0:imgCrop.shape[1]] = imgCrop
-        
-        aspectRatio = h/w
-        if aspectRatio > 1:
-            k = imageSize / h
-            wCal = math.ceil(k * w)
-            imgResize = cv2.resize(imgCrop, (wCal, imageSize))
-            imgResizeShape = imgResize.shape 
-            wGap = math.ceil(imageSize - wCal) // 2
-            imgWhite[:,wGap:wCal+wGap] = imgResize
-            prediction, index = classifier.getPrediction(img)
-            print(prediction, index)
+    if not success:
+        print("Failed to capture image")
+        continue
+
+    
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands_detector.process(imgRGB)
+
+    
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
             
-        else:
-            k = imageSize / w
-            hCal = math.ceil(k * h)
-            imgResize = cv2.resize(imgCrop, (imageSize, hCal))
-            imgResizeShape = imgResize.shape 
-            hGap = math.ceil(imageSize - hCal) // 2
-            imgWhite[hGap:hCal+hGap,:] = imgResize
-        
-        
-        imgCrop = img[y-offset:y+h+offset, x-offset:x+w+offset]
-        cv2.imshow("ImageCrop", imgCrop)
-        cv2.imshow("ImageWhite", imgWhite)
-        
-        
+            
+            h_img, w_img, _ = img.shape
+            x_list = []
+            y_list = []
+
+            for lm in hand_landmarks.landmark:
+                x_list.append(int(lm.x * w_img))
+                y_list.append(int(lm.y * h_img))
+
+            x, y = min(x_list), min(y_list)
+            w, h = max(x_list) - x, max(y_list) - y
+
+            
+            mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+            
+            imgWhite = np.ones((imageSize, imageSize, 3), np.uint8) * 255
+            
+            
+            y1 = max(0, y - offset)
+            y2 = min(h_img, y + h + offset)
+            x1 = max(0, x - offset)
+            x2 = min(w_img, x + w + offset)
+            
+            imgCrop = img[y1:y2, x1:x2]
+
+            
+            if imgCrop.size != 0:
+                imgCropShape = imgCrop.shape
+                aspectRatio = h / w
+
+                try:
+                    if aspectRatio > 1:
+                        
+                        k = imageSize / h
+                        wCal = math.ceil(k * w)
+                        imgResize = cv2.resize(imgCrop, (wCal, imageSize))
+                        wGap = math.ceil((imageSize - wCal) / 2)
+                        
+                       
+                        imgWhite[:, wGap:wCal + wGap] = imgResize
+
+                    else:
+                        
+                        k = imageSize / w
+                        hCal = math.ceil(k * h)
+                        imgResize = cv2.resize(imgCrop, (imageSize, hCal))
+                        hGap = math.ceil((imageSize - hCal) / 2)
+                        
+                        
+                        imgWhite[hGap:hCal + hGap, :] = imgResize
+
+                    cv2.imshow("ImageCrop", imgCrop)
+                    cv2.imshow("ImageWhite", imgWhite)
+
+                except Exception as e:
+                    print(f"Error resizing: {e}")
+
+    
     cv2.imshow("Image", img)
+    
     key = cv2.waitKey(1)
+    
+    
     if key == ord("s"):
         counter += 1
-        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
-        print(counter)
+        filename = f'{folder}/Image_{time.time()}.jpg'
         
         
+        if 'imgWhite' in locals():
+            cv2.imwrite(filename, imgWhite)
+            print(f"Saved: {filename} (Count: {counter})")
+        else:
+            print("No hand detected to save!")
+            
+            
+    if key == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
